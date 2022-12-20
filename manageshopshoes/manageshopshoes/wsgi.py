49b1c0df402f4f0f867f84adad83c44b78a98fbe
truong45truong <58431832+truong45truong@users.noreply.github.com>
2022-12-20 11:05:45 +0700

    Update wsgi.py

--- manageshopshoes/manageshopshoes/wsgi.py
+++ manageshopshoes/manageshopshoes/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+path = '/home/<PythonAnywhere-username>/manageshopshoes'
+if path not in sys.path:
+ sys.path.append(path)
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'manageshopshoes.settings'
+ 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manageshopshoes.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
